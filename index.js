ded58d9ba79b1a51b2809a5de248753a8e873dd7
Whitney Norrad <wnorrad@gmail.com>
2017-11-13 23:28:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function scuberGreetingForFeet(ride) {
   if (ride <= 400) {
-    return 'This one is on me!'
+    return 'This one is on me!';
   } else if (ride > 400 && ride < 2501) {
-    return 'I will gladly take your thirty bucks.'
+    return 'I will gladly take your thirty bucks.';
   } else if (ride > 2500) {
-    return 'No can do.'
+    return 'No can do.';
   }
 }
 
 function ternaryCheckCity(city) {
   if (city == 'NYC') {
-  return 'Ok, sounds good.'
+  return 'Ok, sounds good.';
   } else if (city !== 'NYC') {
-  return 'No go.'
+  return 'No go.';
  }
 }
 

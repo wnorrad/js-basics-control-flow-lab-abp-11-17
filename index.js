39aed05a2351f7ae84e643a2674d6638d0649db6
Whitney Norrad <wnorrad@gmail.com>
2017-11-13 23:53:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!';
+  } else if (ride > 400 && ride < 2501) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (ride > 2500) {
+    return 'No can do.';
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city == 'NYC') {
+  return 'Ok, sounds good.';
+  } else if (city !== 'NYC') {
+  return 'No go.';
+ }
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return 'Thank you so much.';
+  
+    case "not as generous":
+      return 'Thank you.';
+      
+    default:
+      return 'Bye.';
+  
+  }
+}

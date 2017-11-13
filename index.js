20a644cf13d879799bb9fb02b79e53251bde802e
Whitney Norrad <wnorrad@gmail.com>
2017-11-13 23:37:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,5 @@ function switchOnCharmFromTip(tip) {
   switch(tip) {
     case "generous":
       return 'Thank you so much.';
-      break;
-    case "not as generous":
-      return 'Thank you.';
-      break;
-    case ""
   }
 }

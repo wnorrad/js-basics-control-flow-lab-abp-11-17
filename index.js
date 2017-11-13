0c5309d51f594b6442321b865c85a0ae23000873
Whitney Norrad <wnorrad@gmail.com>
2017-11-13 23:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ function ternaryCheckCity(city) {
  }
 }
 
+function switchOnCharmFromTip(tip) {
+  if (tip > 40) {
+    return 'Thank you so much.';
+  } else if (tip > 5 && < 40) {
+    return 'Thank you.'
+  } else if (tip)
+}
+}

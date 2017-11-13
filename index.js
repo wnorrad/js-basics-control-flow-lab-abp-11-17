6a29fae56e7c4fa1da20c1968da7ace6d1567ff7
Whitney Norrad <wnorrad@gmail.com>
2017-11-13 23:17:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code in this file!
+function scuberGreetingForFeet(ride) {
+  if (ride <= 400) {
+    return 'This one is on me!'
+  } else if (ride > 400 && ride < 2501) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city = 'NYC') {
+  return 'Ok, sounds good.'
+  } else if () {
+  return 'No go.'
+ }
+}
